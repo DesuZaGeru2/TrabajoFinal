0c6db7ecbe077f204b8abbcb63c6f2da25b171fd
DesuZaGeru2 <desuzageru2@gmail.com>
2023-04-08 04:04:42 -0300

    finalizados los formularios de registro e ingreso, agregado json dde usuarios y cursos, funcion de tarjetas de cursos lista, implementacion de librerias toastify y sweet alert

--- js/scriptIngreso.js
+++ js/scriptIngreso.js
@@ -1,34 +1,48 @@
 /* -------------------------------INGRESO------------------------------------------- */
 
 const formularioIngreso = document.getElementById("formularioIngreso")
-const user = []
-const saludarUsuario = () =>{
-    ingresoSaludo.innerHTML = `
-    Bienvenido ${user[0].email}
-    `
-}
-
+let userActive = false
+let userName = ""
 
 formularioIngreso.addEventListener("submit", (event) =>{
     event.preventDefault()
-    user.push({
-        
-        email: formularioIngreso.emailIngreso.value
-        /* contrasenia: formularioIngreso.contraseniaIngreso.value */
-        
-    })
-    
-    
+   
+    fetch("./js/usuarios.json")
+        .then(response => response.json())
+        .then(data => {
+            
+            for (const usuario of data){
+                /* verifica que el usuario y la contraseña concuerden con alguno registrado en el json */
+                if(usuario.email == formularioIngreso.emailIngreso.value
+                && usuario.contrasenia == formularioIngreso.contraseniaIngreso.value){
+                    userName = usuario.nombre 
+                    userActive = true
+                }
+            }
+            if (userActive){
+                Toastify({
+            
+                    text: "Bienvenido de nuevo " + userName + "!",
+                    
+                    duration: 3000
+                    
+                    }).showToast();
+                    formularioIngreso.reset()
+                    setTimeout(()=>{ /* Con esto redirigimos al usuario al index en 3 segundos, para darle 
+                    tiempo de aparición a la tostada */
+                        window.location.href = 'index.html';
+                    },3000)
+            }else{
+                Swal.fire( /* en caso de que no se hayan encontrado coincidencias, se muestr esta alerta */
+                    'Algo salió mal',
+                    'E-mail o contraseña incorrecta',
+                    'error',
+                )
+                  formularioIngreso.reset()
+            }
+        })
+   
+ })
+   
 
-    /* for (const usuario of usuarios){
-        if(formularioIngreso.emailIngreso == usuario.email && formularioIngreso.contraseniaIngreso == usuario.contrasenia){
-            alert("Se ha logueado con exito!")
-        }else{
-            alert("Usuario no registrado")
-        }
-    } */
-    formularioIngreso.reset()
 
-    saludarUsuario()
-
-})
---
+++ js/scriptRegistro.js
@@ -0,0 +1,59 @@
+/* -------------------------  REGISTRO ------------------------------------------ */
+const URL_API = "https://restcountries.com/v3.1/all/" //API de paises para selector
+
+    const paisSelect = document.getElementById("selectorPais");
+    
+    fetch(URL_API)
+    .then(response => response.json())
+    .then(data => {
+        const paises = data.map(pais => pais.name.common); // creamos un array con los nombres comunes de los países
+        paises.sort(); // ordenamos los nombres alfabéticamente
+        for(const pais of paises){ //bucle for para mostrar los elementos del array 
+            paisSelect.innerHTML +=`
+            <option value="${pais}">${pais}</option>
+            `
+        }
+    });
+
+    const usuarios = []
+    const formularioRegistro = document.getElementById("formulario-registro");
+    
+    const millisecsInYear = 1000 * 60 * 60 * 24 * 365.25 //cantidad de milisegundos en un año
+    let edadUsuario = 0;
+    const hoy = new Date() //fecha actual
+    
+    formularioRegistro.addEventListener("submit", (event) => {
+        event.preventDefault()
+    
+        /* Se hace la diferencia en milisegundos de la fecha actual y la fecha de nacimiento, 
+        luego se divide por los milisegundos que hay en un año para obtener la edad. Math.floor redondea el nro */
+        edadUsuario = Math.floor((hoy - new Date(formularioRegistro.fechaNac.value)) / millisecsInYear)
+        
+        if(edadUsuario < 18) {
+            Swal.fire(
+                'Algo salió mal',
+                'No es posible registrar a un usuario menor de edad',
+                'error'
+              )
+        }else{
+            usuarios.push({
+                nombre: formularioRegistro.nombre.value,
+                apellido: formularioRegistro.apellido.value,
+                email: formularioRegistro.email.value,
+                contrasenia: formularioRegistro.contrasenia.value,
+                fechaNac: formularioRegistro.fechaNac.value,
+                pais: formularioRegistro.selectorPais.value
+            })
+            Swal.fire(
+                'Registro exitoso!',
+                'En breve llegará un correo a tu E-mail, sigue los pasos para continuar',
+                'success'
+              )
+    
+        }
+        
+        formularioRegistro.reset()
+    })
+    
+
+/*     ---------------------------------------------------------------------------- */
