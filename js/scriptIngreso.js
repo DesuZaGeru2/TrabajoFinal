f3b0643a514cb040aff88a5b836ea3cdc3db5c62
DesuZaGeru2 <desuzageru2@gmail.com>
2023-04-13 23:04:14 -0300

    agregado js de formulario contacto, agregado texto de about us y fondo

--- js/scriptIngreso.js
+++ js/scriptIngreso.js
@@ -4,45 +4,42 @@ const formularioIngreso = document.getElementById("formularioIngreso")
 let userActive = false
 let userName = ""
 
-formularioIngreso.addEventListener("submit", (event) =>{
+formularioIngreso.addEventListener("submit", (event) => {
     event.preventDefault()
-   
+
     fetch("./js/usuarios.json")
         .then(response => response.json())
         .then(data => {
-            
-            for (const usuario of data){
+
+            for (const usuario of data) {
                 /* verifica que el usuario y la contraseña concuerden con alguno registrado en el json */
-                if(usuario.email == formularioIngreso.emailIngreso.value
-                && usuario.contrasenia == formularioIngreso.contraseniaIngreso.value){
-                    userName = usuario.nombre 
+                if (usuario.email == formularioIngreso.emailIngreso.value
+                    && usuario.contrasenia == formularioIngreso.contraseniaIngreso.value) {
+                    userName = usuario.nombre
                     userActive = true
                 }
             }
-            if (userActive){
+            if (userActive) {
                 Toastify({
-            
                     text: "Bienvenido de nuevo " + userName + "!",
-                    
                     duration: 3000
-                    
-                    }).showToast();
+                }).showToast();
                     formularioIngreso.reset()
-                    setTimeout(()=>{ /* Con esto redirigimos al usuario al index en 3 segundos, para darle 
-                    tiempo de aparición a la tostada */
-                        window.location.href = 'index.html';
-                    },3000)
-            }else{
+                    setTimeout(() => { /* Con esto redirigimos al usuario al index en 3 segundos, para darle 
+                      tiempo de aparición a la tostada */
+                      window.location.href = 'index.html';
+                    }, 3000)
+            } else {
                 Swal.fire( /* en caso de que no se hayan encontrado coincidencias, se muestr esta alerta */
                     'Algo salió mal',
                     'E-mail o contraseña incorrecta',
                     'error',
                 )
-                  formularioIngreso.reset()
+                formularioIngreso.reset()
             }
         })
-   
- })
+
+})
    
 
 
